dd4f80f4c389493a0adad61eec84c6a9674a2668
Allison File <144496836+Allison-File@users.noreply.github.com>
2026-06-16 22:06:19 -0400

    Delete WEC-Sim/wecSimInputFile.m

--- WEC-Sim/wecSimInputFile.m
+++
@@ -1,6 +0,0 @@
-% calls custom WEC-Sim input file for certain vehicle configurations (ACV
-%   or turbine-generator PTO)
-
-%wecSimInputFile_p3_1m_ACV
-wecSimInputFile_p3_1m_TG
-%wecSimInputFile_p3_1m_springDamper_1D
